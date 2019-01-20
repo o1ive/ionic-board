b10e033b5bb6c5becbd9f3aaa24b7c69d35572b2
o1ive <o1ive3220@gmail.com>
2019-01-20 17:11:12 +0900

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,73 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  post: {
+    userName: string,
+    message: string,
+    createdDate: any
+  };
+  message: string;
 
+  posts: {userName: string, message: string, createdDate: any }[]
+    = [
+      {
+        userName: '鈴木ヒナ',
+        message: 'ヒメのおっちょこちょい',
+        createdDate: '10分前'
+      },
+      {
+        userName: '田中ヒメ',
+        message: 'ヒナのど天然',
+        createdDate: '2分前'
+      }
+    ];
+
+  constructor(private alertCtrl: AlertController) {}
+
+  addPost() {
+    // 入力されたメッセージを使って、投稿データを作成
+    this.post = {
+      userName: '道明寺ここあ',
+      message: this.message,
+      createdDate: '数秒前'
+    };
+    // 配列postsにpostを追加する
+    this.posts.push(this.post);
+    // 入力フィールドを空にする
+    this.message = '';
+  }
+
+  async presentPrompt(index: number) {
+    const alert = await this.alertCtrl.create({
+      header: 'メッセージ編集',
+      inputs: [
+        {
+          name: 'message',
+          type: 'text',
+          placeholder: 'メッセージ'
+        }
+      ],
+      buttons: [
+        {
+          text: 'キャンセル',
+          role: 'cancel',
+          handler: () => {
+            console.log('キャンセルが選択されました');
+          }
+        },
+        {
+          text: '更新',
+          handler: data => {
+            console.log(data);
+            this.posts[index].message = data.message;
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  deletePost(index: number) {
+    this.posts.splice(index, 1);
+  }
 }
